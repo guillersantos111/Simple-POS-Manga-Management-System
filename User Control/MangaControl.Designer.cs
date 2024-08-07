5524a533942c6c47090277f732a4f1f1299a00ee
Guiller Santos <155286241+guillersantos111@users.noreply.github.com>
2024-08-07 21:19:26 +0800

    Add files via upload

---
+++ User Control/MangaControl.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace PosoManagementSystem.User_Control
+{
+    partial class MangaControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pb_CoverImage = new PictureBox();
+            lbl_Price = new Label();
+            panel = new Panel();
+            lbl_ID = new Label();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pb_CoverImage).BeginInit();
+            panel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // pb_CoverImage
+            // 
+            pb_CoverImage.Cursor = Cursors.Hand;
+            pb_CoverImage.Location = new Point(15, 12);
+            pb_CoverImage.Margin = new Padding(4, 3, 4, 3);
+            pb_CoverImage.Name = "pb_CoverImage";
+            pb_CoverImage.Size = new Size(216, 245);
+            pb_CoverImage.SizeMode = PictureBoxSizeMode.StretchImage;
+            pb_CoverImage.TabIndex = 0;
+            pb_CoverImage.TabStop = false;
+            pb_CoverImage.Click += pb_CoverImage_Click_1;
+            // 
+            // lbl_Price
+            // 
+            lbl_Price.AutoSize = true;
+            lbl_Price.Cursor = Cursors.Hand;
+            lbl_Price.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lbl_Price.Location = new Point(88, 5);
+            lbl_Price.Margin = new Padding(4, 0, 4, 0);
+            lbl_Price.Name = "lbl_Price";
+            lbl_Price.Size = new Size(13, 19);
+            lbl_Price.TabIndex = 3;
+            lbl_Price.Text = " ";
+            lbl_Price.Click += lbl_Price_Click;
+            // 
+            // panel
+            // 
+            panel.Controls.Add(lbl_Price);
+            panel.Location = new Point(11, 257);
+            panel.Name = "panel";
+            panel.Size = new Size(224, 28);
+            panel.TabIndex = 4;
+            // 
+            // lbl_ID
+            // 
+            lbl_ID.AutoSize = true;
+            lbl_ID.BackColor = Color.Transparent;
+            lbl_ID.Cursor = Cursors.Hand;
+            lbl_ID.Font = new Font("Arial Black", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lbl_ID.Location = new Point(3, 3);
+            lbl_ID.Margin = new Padding(4, 0, 4, 0);
+            lbl_ID.Name = "lbl_ID";
+            lbl_ID.Size = new Size(15, 23);
+            lbl_ID.TabIndex = 4;
+            lbl_ID.Text = " ";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Cursor = Cursors.Hand;
+            pictureBox1.Image = Properties.Resources.animetrix;
+            pictureBox1.Location = new Point(197, 0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(48, 46);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 5;
+            pictureBox1.TabStop = false;
+            // 
+            // MangaControl
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            Controls.Add(lbl_ID);
+            Controls.Add(pictureBox1);
+            Controls.Add(panel);
+            Controls.Add(pb_CoverImage);
+            Margin = new Padding(15);
+            Name = "MangaControl";
+            Size = new Size(245, 298);
+            ((System.ComponentModel.ISupportInitialize)pb_CoverImage).EndInit();
+            panel.ResumeLayout(false);
+            panel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_Price;
+        private System.Windows.Forms.Panel panel;
+        private System.Windows.Forms.PictureBox pb_CoverImage;
+        private PictureBox pictureBox1;
+        private Label lbl_ID;
+    }
+}
---
+++ User Control/MangaControl.cs
@@ -0,0 +1,61 @@
+﻿using PosoManagementSystem.Model;
+using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace PosoManagementSystem.User_Control
+{
+    public partial class MangaControl : UserControl
+    {
+        private MangaModels mangaModels;
+
+        public MangaControl(MangaModels mangaModels)
+        {
+            InitializeComponent();
+            this.mangaModels = mangaModels;
+            LoadMangaDetails();
+        }
+
+        private void LoadMangaDetails()
+        {
+            lbl_ID.Text = $"#{mangaModels.MangaID}";
+            lbl_Price.Text = $" {mangaModels.Price:C}";
+
+            if (mangaModels.CoverImage != null)
+            {
+                using (MemoryStream ms = new MemoryStream(mangaModels.CoverImage))
+                {
+                    pb_CoverImage.Image = Image.FromStream(ms);
+                }
+            }
+        }
+
+        private void pb_CoverImage_Click_1(object sender, EventArgs e)
+        {
+            OnMangaClicked?.Invoke(mangaModels);
+            OnAddToCart?.Invoke(mangaModels);
+        }
+
+        private void lbl_Price_Click(object sender, EventArgs e)
+        {
+            OnMangaClicked?.Invoke(mangaModels);
+            OnAddToCart?.Invoke(mangaModels);
+        }
+
+        private void btn_Update(object sender, EventArgs e)
+        {
+            OnUpdateClicked?.Invoke(mangaModels);
+        }
+
+        private void btn_Delete(object sender, EventArgs e)
+        {
+            OnDeleteClicked?.Invoke(mangaModels);
+        }
+
+        public event Action<MangaModels> OnMangaClicked;
+        public event Action<MangaModels> OnUpdateClicked;
+        public event Action<MangaModels> OnDeleteClicked;
+        public event Action<MangaModels> OnAddToCart;
+    }
+}
